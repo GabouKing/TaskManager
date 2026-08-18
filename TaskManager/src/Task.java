880e5346cb081d00a421806fc2794966b1ec32ca
Gabriel Ribeiro <gribeirodelimas2@outlook.com>
2026-08-18 16:36:05 -0300

    feat: add create task flow

--- TaskManager/src/Task.java
+++ TaskManager/src/Task.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String decription;
+    private String Status;
+
+    public Task(String decription, String status) {
+        this.decription = decription;
+        Status = status;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
 }
--- TaskManager/src/TaskRepository.java
+++ TaskManager/src/TaskRepository.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TaskRepository {
+    private static final String ARCHIVE = "tasks.csv";
+
+    public void Save(Task task) {
+        try (FileWriter writer = new FileWriter(ARCHIVE, true)){
+
+            writer.write(
+                    task.getDecription()
+                    +","
+                    +task.getStatus()
+            );
+
+        } catch(IOException exception){
+            System.out.println("Error while writing to file whith message: " + exception.getMessage());
+        }
+    }
 }
